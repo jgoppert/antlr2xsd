ca9c23b53f3dcc00a3a509edc81ca1aa1b955a1b
James Goppert <james.goppert@gmail.com>
2018-03-22 19:52:21 -0400

    Cleanup.

--- src/antlr2xsd/__init__.py
+++ src/antlr2xsd/__init__.py
@@ -1,3 +1,6 @@
+"""
+Converts ANTLR g4 gramamr files into xml XSD schemas.
+"""
 import os
 import sys
 from math import inf
@@ -6,7 +9,6 @@ import antlr4
 
 from lxml import etree
 
-# import json
 sys.path.append(os.path.pardir)
 from antlr2xsd.generated.ANTLRv4Lexer import ANTLRv4Lexer  # noqa: E402, I100
 from antlr2xsd.generated.ANTLRv4Parser import ANTLRv4Parser  # noqa: E402
@@ -14,6 +16,10 @@ from antlr2xsd.generated.ANTLRv4ParserListener import ANTLRv4ParserListener  # n
 
 
 class Listener(ANTLRv4ParserListener):
+    """
+    Parses g4 file into a JSON structure
+    with info for XSD
+    """
 
     def __init__(self):
         self.scope = {
@@ -25,20 +31,20 @@ class Listener(ANTLRv4ParserListener):
         self.src = {}
 
     def enterGrammarSpec(self, ctx: ANTLRv4Parser.GrammarSpecContext):
-        e = {
+        elem = {
             'rules': []
         }
-        self.scope['root'] = e
-        self.src[ctx] = e
+        self.scope['root'] = elem
+        self.src[ctx] = elem
 
     def enterParserRuleSpec(self, ctx: ANTLRv4Parser.ParserRuleSpecContext):
-        e = {
+        elem = {
             'name': str(ctx.RULE_REF()),
             'refs': {}
         }
         self.scope['ebnf'].append({})
-        self.scope['rule'] = e
-        self.src[ctx] = e
+        self.scope['rule'] = elem
+        self.src[ctx] = elem
 
     def exitParserRuleSpec(self, ctx: ANTLRv4Parser.RuleSpecContext):
         ebnf = self.scope['ebnf'][-1]
@@ -54,16 +60,16 @@ class Listener(ANTLRv4ParserListener):
             suffix = self.src[ctx.blockSuffix()]
             refs = self.scope['ebnf'][-1]
             if suffix == "?":
-                for r in refs:
-                    refs[r]['min'] = 0
+                for ref in refs:
+                    refs[ref]['min'] = 0
             elif suffix == "+":
-                for r in refs:
-                    refs[r]['min'] += 1
-                    refs[r]['max'] = inf
+                for ref in refs:
+                    refs[ref]['min'] += 1
+                    refs[ref]['max'] = inf
             elif suffix == "*":
-                for r in refs:
-                    refs[r]['min'] *= 0
-                    refs[r]['max'] *= inf
+                for ref in refs:
+                    refs[ref]['min'] *= 0
+                    refs[ref]['max'] *= inf
         inner = self.scope['ebnf'].pop()
         outer = self.scope['ebnf'][-1]
         for k in inner.keys():
@@ -95,10 +101,15 @@ class Listener(ANTLRv4ParserListener):
         }
 
     def exitRuleAltList(self, ctx: ANTLRv4Parser.RuleAltListContext):
+        # TODO add choice
         pass
 
 
 def parse_g4(g4_path):
+    """
+    Parse a g4 file and return a JSON structure with
+    data for XSD
+    """
     input_stream = antlr4.FileStream(g4_path)
     lexer = ANTLRv4Lexer(input_stream)
     stream = antlr4.CommonTokenStream(lexer)
@@ -111,21 +122,25 @@ def parse_g4(g4_path):
 
 
 def to_xsd(data):
+    """
+    Take a json XML file from parse_g4 and
+    convert it to an XSD
+    """
     root = etree.Element('schema')
     for rule in data['rules']:
-        type = etree.Element(
+        rule_type = etree.Element(
             'complex_type',
-            name=rule['name'])  # type: etree._Element
+            name=rule['name'])  # rule_type: etree._Element
         for ref_name, ref in rule['refs'].items():
-            type.append(etree.Element('element', name=ref_name, type=ref_name))
-            min = str(ref['min'])
+            rule_type.append(etree.Element('element', name=ref_name, rule_type=ref_name))
+            min_val = str(ref['min'])
             if ref['max'] == inf:
-                max = 'unbounded'
+                max_val = 'unbounded'
             else:
-                max = str(ref['max'])
-            if min == '1' and max == '1':
+                max_val = str(ref['max'])
+            if min_val == '1' and max_val == '1':
                 continue
-            type.attrib['min_occurs'] = min
-            type.attrib['max_occurs'] = max
+            rule_type.attrib['min_occurs'] = min_val
+            rule_type.attrib['max_occurs'] = max_val
         root.append(type)
     return root
